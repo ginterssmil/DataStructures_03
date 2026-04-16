09b13fd7a85f5c5b111c0745522f95ce91dead55
ginterssmil <smiltinsginters@example.com>
2026-04-16 12:13:51 +0300

    MyLinkedHeap testing Main service, started dequeue funkc

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,10 +1,27 @@
 package service;
 
+import datastr.MyLinkedHeap;
+
 public class MainService {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MyLinkedHeap<Integer> integerInHeap = new MyLinkedHeap<Integer>();
+		try {
+		integerInHeap.enqueue(40);
+		integerInHeap.enqueue(50);
+		integerInHeap.enqueue(35);
+		integerInHeap.enqueue(90);
+		integerInHeap.enqueue(55);
+		integerInHeap.print();
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
+		
+		
+		
 	}
 
 }
