9b97d5b2db49370c95d8573da384d6d60e3b8cae
ginterssmil <smiltinsginters@example.com>
2026-04-16 11:48:13 +0300

    MyLinkedHeap  Mosttly done

---
+++ src/datastr/MyLinkedHeap.java
@@ -0,0 +1,178 @@
+package datastr;
+
+public class MyLinkedHeap<Ttype> {
+	//1.mainigie
+	private MyNode<Ttype> rootNode = null;
+	private MyNode<Ttype> lastNode = null;
+	private int howManyElements = 0;
+	private int level = 0;
+	
+	//2.getter, priekš blokiem nav nepieciešams
+	public int getHowManyElements()
+	{
+		return howManyElements;
+	}
+	//3.setter - nav nepieciesams nevienam mainigajam
+	
+	//4.konstruktors/i - bezargumenta konstruktors būs no Object klases
+	
+	public boolean isEmpty() {
+		return (howManyElements==0);
+	}
+	
+	public boolean isFull() {
+		try {
+			new MyNode<Character>('A');
+			return false;
+		}
+		catch (OutOfMemoryError e) {
+			return true;
+		}
+	}
+	
+	
+	public void enqueue(Ttype element) throws Exception {
+		if(isFull()) {
+			throw new Exception(
+					"Kaudze ir pilna un nav iespējams pievienot elementu");
+		}
+		
+		if(element == null) {
+			throw new Exception("Elements nevar būt null");
+		}
+		
+		if(isEmpty()) {//ja tiek pievienots pirmais elements
+			MyNode<Ttype> newNode = new MyNode<Ttype>(element);
+			rootNode = newNode;
+			lastNode = newNode;
+			howManyElements++;
+		}
+		else//ja tiek pievienots kārtējais ( ne pirmais) elements
+		{
+			MyNode<Ttype> newNode = new MyNode<Ttype>(element);
+			//ja būs saknes elementam kreisais bērns
+			if(howManyElements == 1) {
+				rootNode.setLeftChNode(newNode);
+				newNode.setParentNode(rootNode);
+				lastNode = newNode;
+				howManyElements++;
+				level++;
+				reheapUp(newNode);
+				return;
+			}
+			
+			
+			
+			
+			
+			//pēdējam blokam nav neviens no bērniem
+			if(lastNode.getLeftChNode()==null && lastNode.getRightChNode()==null) {
+				lastNode.setLeftChNode(newNode);
+				newNode.setParentNode(lastNode);
+				lastNode = newNode;
+				howManyElements++;
+				reheapUp(newNode);
+				return;
+			}
+			
+			//kad pedjeam blokam nav blakus labais bloks
+			if(lastNode.getParentNode()!=null 
+					&& lastNode.getParentNode().getRightChNode()==null) {
+				
+				MyNode<Ttype> parentNodeTemp = lastNode.getParentNode();
+				parentNodeTemp.setRightChNode(newNode);
+				newNode.setParentNode(parentNodeTemp);
+				
+				lastNode = newNode;
+				howManyElements++;
+				reheapUp(newNode);
+				return;
+				
+			}
+			
+			//2^0 = 1 elements 0.līmenī
+			//2^1 = 2 elementi 1.līmenī
+			//2^2 = 4 elementi 2.līmenī
+			int sum = 0;
+			//es noskaidroju, cik ir jābūt blokiem līdz šim līmenim ieskaitot
+			for(int i = 0; i <= level; i++) {
+				sum = (int) (sum + Math.pow(2, i));
+			}
+			//lastNode ir kā pēdejais bloks sava līmenī
+			if(sum == howManyElements) {
+				MyNode<Ttype> currentNode = rootNode;
+				
+				//ja blokam ir kreisais berns, tad jelec uz to
+				while(currentNode.getLeftChNode()!=null) {
+					currentNode = currentNode.getLeftChNode();
+				}
+				
+				lastNode = currentNode;
+				
+				lastNode.setLeftChNode(newNode);
+				newNode.setParentNode(lastNode);
+				
+				lastNode = newNode;
+				howManyElements++;
+				level++;
+				reheapUp(newNode);
+				return;
+				
+			}
+			//TODO izveidot pedējo scenāriju, kurs no labā bērna spej pārlekt
+			//uz blakus apkaškoka kreiso bērnu
+			
+		}
+		
+	}
+	
+	//MAX kaudzes gadījums
+	private void reheapUp(MyNode<Ttype> nodeTemp) {
+		//vai blokam ir vecāks
+		if(nodeTemp.getParentNode()!=null) {
+			MyNode<Ttype> parentTempNode = nodeTemp.getParentNode();
+			//salidzinam, vai bloka vertiba gadījuma nav lielāka par vecak vērtību
+			if(((Comparable)nodeTemp.getElement())
+					.compareTo(parentTempNode.getElement()) > 0) {
+				//mainam vietā vērtības
+				swap(nodeTemp, parentTempNode);
+				reheapUp(parentTempNode);//izsaucam so pašu funkciju rekursīvi, bet jau uz vacaka bloku
+			}
+		}
+	}
+	
+	private void swap(MyNode<Ttype> node1, MyNode<Ttype> node2) {
+		Ttype temp = node1.getElement();
+		node1.setElement(node2.getElement());
+		node2.setElement(temp);
+		
+	}
+	
+	
+	public void print() throws Exception {
+		if(isEmpty() ) {
+			throw new Exception("Kaudze ir tuksa un to nevar izprintet");
+		}
+		
+		printHelper(rootNode);
+	}
+	
+	private void printHelper(MyNode<Ttype> nodeTemp) {
+		if(nodeTemp!=null) {
+			System.out.println("P: "+ nodeTemp.getElement());
+			//vai eksiste kreisais
+			if(nodeTemp.getLeftChNode()!=null) {
+				System.out.println("P: "+ nodeTemp.getElement()
+				+ " Left child: " + nodeTemp.getLeftChNode().getElement());
+				printHelper(nodeTemp.getLeftChNode());
+			}
+			//vai eksiste labais
+			if(nodeTemp.getRightChNode()!=null) {
+				System.out.println("P: "+ nodeTemp.getElement()
+				+ " Right child: " + nodeTemp.getRightChNode().getElement());
+				printHelper(nodeTemp.getRightChNode());
+			}
+		}
+	}
+	
+}
--- src/datastr/MyNode.java
+++ src/datastr/MyNode.java
@@ -1,5 +1,55 @@
 package datastr;
 
-public class MyNode {
+public class MyNode<Ttype> {
 
+	private Ttype element;
+	private MyNode<Ttype> parentNode = null;
+	private MyNode<Ttype> leftChNode = null;
+	private MyNode<Ttype> rightChNode = null;
+	
+
+	public Ttype getElement() {
+		return element;
+	}
+	public MyNode<Ttype> getParentNode() {
+		return parentNode;
+	}
+	public MyNode<Ttype> getLeftChNode() {
+		return leftChNode;
+	}
+	public MyNode<Ttype> getRightChNode() {
+		return rightChNode;
+	}
+	
+
+	public void setElement(Ttype inputElement) {
+		if(inputElement!=null)
+		{
+			element = inputElement;
+		}
+		else
+		{
+			element = (Ttype)new Object();
+		}
+	}
+	public void setParentNode(MyNode<Ttype> parentNode) {
+		this.parentNode = parentNode;
+	}
+	public void setLeftChNode(MyNode<Ttype> leftChNode) {
+		this.leftChNode = leftChNode;
+	}
+	public void setRightChNode(MyNode<Ttype> rightChNode) {
+		this.rightChNode = rightChNode;
+	}
+	
+	
+
+	public MyNode(Ttype inputElement){
+		setElement(inputElement);
+	}
+	
+
+	public String toString() {
+		return "" + element;
+	}
 }
